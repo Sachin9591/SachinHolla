24ef96f3c0d1cfe6c955e0cbd36f4c2385bfbb4d
Admin <skumar10031993@gmail.com>
2022-10-11 17:35:17 +0530

    app password modified

--- SachinHolla/settings.py
+++ SachinHolla/settings.py
@@ -130,7 +130,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'skumar10031993@gmail.com'
-EMAIL_HOST_PASSWORD = 'ohvswawqirufweeh'
+EMAIL_HOST_PASSWORD = 'iaamykwgaecxzvhk'
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
